f687cd59f5d9a4f6f148e4060d53d585950fc809
bmouse1006 <bmouse1006@msn.com>
2012-05-28 08:04:23 +0800

    0528

---
+++ BreezyReader2/Classes/ViewsAndControllers/SettingViewController/BRAccountSettingCell.h
@@ -0,0 +1,13 @@
+//
+//  BRAccountSettingCell.h
+//  BreezyReader2
+//
+//  Created by 金 津 on 12-5-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BRSettingCell.h"
+
+@interface BRAccountSettingCell : BRSettingCell
+
+@end
---
+++ BreezyReader2/Classes/ViewsAndControllers/SettingViewController/BRAccountSettingCell.m
@@ -0,0 +1,31 @@
+//
+//  BRAccountSettingCell.m
+//  BreezyReader2
+//
+//  Created by 金 津 on 12-5-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BRAccountSettingCell.h"
+
+@implementation BRAccountSettingCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
