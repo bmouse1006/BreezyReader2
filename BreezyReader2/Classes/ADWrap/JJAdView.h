05c923f7e6ceb7c0d2939ca4aba2e799720131ad
Jin Jin <bmouse1006@msn.com>
2012-06-27 15:48:06 +0800

    no message

---
+++ BreezyReader2/Classes/ADWrap/JJAdView.h
@@ -0,0 +1,13 @@
+//
+//  JJAdView.h
+//  BreezyReader2
+//
+//  Created by 津 金 on 12-6-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAdView : UIView
+
+@end
---
+++ BreezyReader2/Classes/ADWrap/JJAdView.m
@@ -0,0 +1,31 @@
+//
+//  JJAdView.m
+//  BreezyReader2
+//
+//  Created by 津 金 on 12-6-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "JJAdView.h"
+
+@implementation JJAdView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
